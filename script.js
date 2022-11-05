e1ba603691fcebf68550647e3124fe5d89c6970f
Carlos Alberto <92316071+MarkingCarlos@users.noreply.github.com>
2022-11-05 13:33:37 -0300

    Update Js and Style
    Adding new features

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 let isExplorer = true
 function Card(event) {
   const card = event.currentTarget
+  const img = card.querySelector('img')
   const backgroundImage = isExplorer ? 'url(./assets/bg-ignite.svg)' : 'url(./assets/bg-explorer.svg)'
+  const border = isExplorer ? '4px solid #CDEB14' : '4px solid #2BC8BE'
   isExplorer = !isExplorer
+  
   card.style.backgroundImage = backgroundImage
+  img.style.border = border
+  
 }
